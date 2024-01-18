e25327cbfa950f565db64477edd9109d4145d5b6
xycy30pi <yigit.alicikus@st.ovgu.de>
2024-01-18 16:08:29 +0100

    there is no random numbers and buttons still push randomly

--- task-08-game/main.c
+++ task-08-game/main.c
@@ -11,49 +11,51 @@
 #include "ztimer.h"
 #include "random.h"
 
+#define MSG_QUEUE_SIZE 2
+
 static gpio_t buttons[] = { BTN0_PIN, BTN1_PIN, BTN2_PIN, BTN3_PIN };
-//static gpio_mode_t button_modes[] = { BTN0_MODE, BTN1_MODE, BTN2_MODE, BTN3_MODE };
 static gpio_t leds[] = { LED0_PIN, LED1_PIN, LED2_PIN, LED3_PIN };
 
 char thread_stack[THREAD_STACKSIZE_LARGE];
-char thread_stack_2[THREAD_STACKSIZE_LARGE];
 
-mutex_t mut;
-int indexes[10];
-int is_given = 0;
 kernel_pid_t main_pid;
 
 static void init_gpios(void)
 {
     for (unsigned i = 0; i < ARRAY_SIZE(buttons); i++) {
-        gpio_init(buttons[i], GPIO_IN_PD);
+        gpio_init(buttons[i], GPIO_IN);
     }
 
     for (unsigned i = 0; i < ARRAY_SIZE(leds); i++) {
-        gpio_init(leds[i], GPIO_OUT);
+        gpio_init(leds[i], GPIO_OD_PU);
     }
+}
+static void set_pid(void) {
+    main_pid = thread_getpid();
 }
 
 void* record_buttons(void* _arg)
 {
-    mutex_lock(&mut);
+    msg_t msg;
 
     int diff = (int)_arg;
-    int records[10];
+    int* records = (int*)malloc(5*diff*sizeof(int)); //difficulty 1 => 5 integer(light), difficulty 2 => 10 integer(light)
 
     int inp;
     int c = 0;
     while (1) {
-        
         if(c >= 5*diff) {
-            memcpy(indexes, records, sizeof(int)*5*diff);
-            is_given = 1;
-            mutex_unlock(&mut);
+            printf(" mesaj gonderdim");
+            msg.content.ptr = (void*)records;
+            if(msg_send(&msg, main_pid) > 0) {
+                printf("  Basarili");
+            }
+            break;
         }
         for (int i = 0; i < 4; i++) {
             inp = gpio_read(buttons[i]);
             if(inp > 0) {
-                printf("%d", i);
+                printf("%d ", i);
                 records[c] = i + 1;
                 c++;
             }
@@ -66,26 +68,50 @@ void* record_buttons(void* _arg)
 
 void* light_leds(int difficulty, uint32_t lights[]) {
     
-    for(int i = 0; i < 5*difficulty; i++) {
-        gpio_set(leds[lights[i] - 1]);
-        ztimer_sleep(ZTIMER_MSEC, 500);
-        gpio_write(leds[lights[i] - 1], 0);
-    }
-    
+        for(int i = 0; i < 5*difficulty; i++) {
+                switch (lights[i]) {
+            case 1:
+                LED0_ON;
+                ztimer_sleep(ZTIMER_MSEC, 500);
+                LED0_OFF;
+                break;
+            case 2:
+                LED1_ON;
+                ztimer_sleep(ZTIMER_MSEC, 500);
+                LED1_OFF;
+                break;
+            case 3:
+                LED2_ON;
+                ztimer_sleep(ZTIMER_MSEC, 500);
+                LED2_OFF;
+                break;
+            case 4:
+                LED3_ON;
+                ztimer_sleep(ZTIMER_MSEC, 500);
+                LED3_OFF;
+                break;
+            default:
+                puts("Wrong light number");
+                break;
+            }
+        }
+
     return NULL;
 }
 
 int test_level(int difficulty) {  
-    memset(indexes, '\0', sizeof(int) * 10);
-    is_given = 0;
 
-    ztimer_now_t t = ztimer_now(ZTIMER_MSEC);
-    uint32_t lights[10];
+    msg_t msg;
+    msg.content.ptr = NULL;
 
-    for (int i = 0; i < 5*difficulty; i++) {
-        lights[i] = (t % 4) + 1;
-        t = ztimer_now(ZTIMER_MSEC);
-    }
+    //ztimer_now_t t = ztimer_now(ZTIMER_MSEC);
+    uint32_t lights[10] = {1, 2, 3, 4, 1};
+
+    //for (int i = 0; i < 5*difficulty; i++) {
+    //    //lights[i] = (t % 4) + 1;
+    //    //t = ztimer_now(ZTIMER_MSEC);
+    //    lights[i] = random_uint32_range(1, 5);
+    //}
     for(int i = 0; i < 5*difficulty; i++) {
         printf("%lu", lights[i]);
     }
@@ -105,37 +131,42 @@ int test_level(int difficulty) {
     puts("BEGIN! YOU HAVE 3 SECONDS\n");
     thread_create(thread_stack, sizeof(thread_stack), THREAD_PRIORITY_MAIN - 1, 
                     THREAD_CREATE_STACKTEST, record_buttons, (void*)difficulty, "Buttons push recorder");
-    //ztimer_sleep(ZTIMER_SEC, 3);
+    
+    ztimer_msg_receive_timeout(ZTIMER_MSEC, &msg, 3000);
     
 
-    if(is_given == 0) {
+    if(msg.content.ptr == NULL) {
         puts("You are too late :(\n");
         return -1;
     }
+
+    int* response = (int*)msg.content.ptr;
     
     for(int i = 0; i < 5*difficulty; i++) {
-        printf("%d ", indexes[i]);
+        printf("%d ", response[i]);
     }
 
     for (int i = 0; i < 5*difficulty; i++) {
-        if(indexes[i] == (int)lights[i]) {
+        if(response[i] == (int)lights[i]) {
             continue;
         }
         puts("You have failed!\n");
         return -1;
     }
     puts("YOU HAVE WON!!!\n");
+    free(msg.content.ptr);
     return 0;
 }
 
 int main(void)
 {
     init_gpios();
-    mutex_init(&mut);  
+    set_pid();
 
-    int diffc = 0;
+    int difficulty = 0;
     int n;
     char inp[2];
+
     puts("Welcome to The Memory Game\n");
 
     while (1) {
@@ -147,9 +178,9 @@ int main(void)
             puts("Please enter 1 or 2!");
             continue;
         }
-        diffc = inp[0] - 48;
+        difficulty = inp[0] - 48;
 
-        test_level(diffc);
+        test_level(difficulty);
 
         while ((n = getchar()) != '\n' && n != EOF);
 
